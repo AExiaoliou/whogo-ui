9c29c8857bc2b23658b07d30a9b7338d143e6e85
Starzzk <1019224925@qq.com>
2021-06-26 16:34:05 +0800

    表格表项创建完成

--- src/views/SysBookmember.vue
+++ src/views/SysBookmember.vue
@@ -3,13 +3,11 @@
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column fixed prop="ID" label="ID" width="50">
       </el-table-column>
-      <el-table-column prop="name" label="书名" width="120"> </el-table-column>
-      <el-table-column prop="count" label="数量" width="120"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
       <el-table-column prop="create_by" label="创建用户" width="150">
       </el-table-column>
       <el-table-column prop="create_time" label="创建时间" width="200"> </el-table-column>
       <el-table-column prop="last_update_time" label="最近更新时间" width="120"> </el-table-column>
-      <el-table-column prop="isbn" label="ISBN" width="300"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small"
--- src/views/SysTeacher.vue
+++ src/views/SysTeacher.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column fixed prop="ID" label="ID" width="50">
+      <el-table-column fixed prop="id" label="ID" width="50">
       </el-table-column>
-      <el-table-column prop="name" label="书名" width="120"> </el-table-column>
-      <el-table-column prop="count" label="数量" width="120"> </el-table-column>
+      <el-table-column prop="user_name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="workid" label="工号" width="120"> </el-table-column>
+      <el-table-column prop="sex" label="性别" width="120"> </el-table-column>
       <el-table-column prop="create_by" label="创建用户" width="150">
       </el-table-column>
       <el-table-column prop="create_time" label="创建时间" width="200"> </el-table-column>
+      <el-table-column prop="last_update_by" label="最近更新时间" width="120"> </el-table-column>
       <el-table-column prop="last_update_time" label="最近更新时间" width="120"> </el-table-column>
-      <el-table-column prop="isbn" label="ISBN" width="300"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text" size="small"
