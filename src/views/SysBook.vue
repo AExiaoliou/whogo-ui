dac3b0fe0536b5d718ebb2642fcbcbdbc46d44c5
Starzzk <1019224925@qq.com>
2021-06-29 09:03:58 +0800

    新增按钮

--- src/views/SysBook.vue
+++ src/views/SysBook.vue
@@ -2,20 +2,17 @@
   <div>
     <el-card>
       <el-table :data="tableData" stripe>
-        <!-- 新增按钮 -->
         <el-table-column fixed prop="ID" width="75">
           <template slot="header">
             <el-button size="small" type="primary">新增...</el-button>
           </template>
         </el-table-column>
-        <!-- 数据表单 -->
         <el-table-column prop="name" label="书名" width="120"> </el-table-column>
         <el-table-column prop="count" label="数量" width="120"> </el-table-column>
         <el-table-column prop="isbn" label="ISBN" width="300"></el-table-column>
         <el-table-column prop="create_by" label="创建用户" width="150"></el-table-column>
         <el-table-column prop="create_time" label="创建时间" width="200"> </el-table-column>
         <el-table-column prop="last_update_time" label="最近更新时间" width="120"> </el-table-column>
-        <!-- 最右搜索栏和操作按钮 -->
         <el-table-column fixed="right" width="150" align="right">
           <template slot="header">
             <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
@@ -28,7 +25,6 @@
       </el-table>
     </el-card>
     <div class="block"></div>
-    <!-- 分页 -->
     <el-pagination
       :page-size="20"
       :pager-count="11"
@@ -46,7 +42,7 @@ export default {
     }
   },
   created () {
-    // 随便找的一个api做的测试, 肯定是错的不用管
+    this.$api.role.findAll().then(res => { this.tableData = res.data })
     this.$api.role.findAll().then((res) => {
       this.tableData = res.data
     })
@@ -54,4 +50,5 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+</style>
--- src/views/SysCafemember.vue
+++ src/views/SysCafemember.vue
@@ -1,20 +1,34 @@
 <template>
   <div>
-    <el-table :data="tableData" stripe>
-      <el-table-column fixed prop="id" label="ID" width="50"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
-      <el-table-column prop="price" label="价格" width="120"> </el-table-column>
-      <el-table-column prop="create_by" label="创建用户" width="150"> </el-table-column>
-      <el-table-column prop="create_time" label="创建时间" width="200"> </el-table-column>
-      <el-table-column prop="last_update_by" label="最近更新用户" width="120"> </el-table-column>
-      <el-table-column prop="last_update_time" label="最近更新时间" width="120"> </el-table-column>
-      <el-table-column fixed="right" label="操作" width="200">
-        <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-card>
+      <el-table :data="tableData" stripe>
+        <el-table-column fixed prop="ID" width="75">
+          <template slot="header">
+            <el-button size="small" type="primary">新增...</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column fixed prop="id" label="ID" width="50"> </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+        <el-table-column prop="price" label="价格" width="120"> </el-table-column>
+        <el-table-column prop="create_by" label="创建用户" width="150"> </el-table-column>
+        <el-table-column prop="create_time" label="创建时间" width="200"> </el-table-column>
+        <el-table-column prop="last_update_by" label="最近更新用户" width="120"> </el-table-column>
+        <el-table-column prop="last_update_time" label="最近更新时间" width="120"> </el-table-column>
+        <el-table-column fixed="right" label="操作" width="200">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+    <div class="block"></div>
+    <el-pagination
+      :page-size="20"
+      :pager-count="11"
+      layout="prev, pager, next"
+      :total="1000">
+    </el-pagination>
   </div>
 </template>
 
