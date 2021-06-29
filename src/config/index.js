8c133ba0cea0e1abea95ca8b9e6fc4435e00ae46
AExiaoliou <aexiaoliou@gmail.com>
2021-06-29 22:02:44 +0800

    config: baseUrl改为动态

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
-
 export default {
-  baseUrl: 'http://localhost:9998',
+  baseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8001' : 'http://119.23.241.152:9999',
   Mock: process.env.NODE_ENV !== 'production'
   // Mock: false
 }
