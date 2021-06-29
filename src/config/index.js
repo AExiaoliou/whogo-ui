4d619296eed3110622de1b1dc8c0c920973048b0
AExiaoliou <aexiaoliou@gmail.com>
2021-06-29 22:11:50 +0800

    ...

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  baseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8001' : 'http://119.23.241.152:9999',
+  baseUrl: process.env.NODE_ENV !== 'production' ? 'http://localhost:8001' : 'http://119.23.241.152:9998',
   Mock: process.env.NODE_ENV !== 'production'
   // Mock: false
 }
