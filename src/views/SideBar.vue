f6817642e390553868cd666903fb2e5d3b29d2ad
Starzzk <1019224925@qq.com>
2021-07-03 16:41:25 +0800

    修改了导航栏

--- src/views/SideBar.vue
+++ src/views/SideBar.vue
@@ -1,15 +1,24 @@
 <template>
   <aside class="Sider">
     <el-menu class="menu" :collapse="isSideCollapse" router>
-      <el-menu-item index="/systeacher">教职工管理</el-menu-item>
-      <el-menu-item index="/sysstudent">学生管理</el-menu-item>
-      <el-menu-item index="/syscafe">食堂菜品管理</el-menu-item>
-      <el-menu-item index="/syscafemember">食堂会员管理</el-menu-item>
-      <el-menu-item index="/sysbook">图书管理</el-menu-item>
-      <el-menu-item index="/sysbookmember">图书馆会员管理</el-menu-item>
-      <el-menu-item index="/user">用户管理</el-menu-item>
-      <el-menu-item index="/monitor">系统监控</el-menu-item>
-      <el-menu-item v-if="testEnv" index="/test">test</el-menu-item>
+      <el-submenu index="1">
+          <template template slot="title">人员管理系统</template>
+          <el-menu-item index="/systeacher">教职工管理</el-menu-item>
+          <el-menu-item index="/sysstudent">学生管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+          <template template slot="title">食堂管理系统</template>
+          <el-menu-item index="/syscafe">食堂菜品管理</el-menu-item>
+          <el-menu-item index="/syscafemember">食堂会员管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="3">
+          <template template slot="title">图书馆管理系统</template>
+          <el-menu-item index="/sysbook">图书管理</el-menu-item>
+          <el-menu-item index="/sysbookmember">图书馆会员管理</el-menu-item>
+      </el-submenu>
+        <el-menu-item index="/user">用户管理</el-menu-item>
+        <el-menu-item index="/monitor">系统监控</el-menu-item>
+        <el-menu-item v-if="testEnv" index="/test">test</el-menu-item>
     </el-menu>
   </aside>
 </template>
@@ -19,12 +28,18 @@ export default {
   name: 'side-bar',
   data () {
     return {
-      testEnv: process.env.NODE_ENV !== 'production'
+      testEnv: process.env.NODE_ENV !== 'production',
+      activeNames: ['1']
     }
   },
   computed: {
     isSideCollapse () {
       return this.$store.state.app.isSideCollapse
+    }
+  },
+  methods: {
+    handleChange (val) {
+      console.log(val)
     }
   }
 }
